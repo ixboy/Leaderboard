3d3cfa145994f88388590a3279a6d1780889596c
ixboy <ismaelixboy@gmail.com>
2021-10-14 23:44:39 +0100

    add function to get data from the api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
 import './style.scss';
 import './main.js';
+
+console.log('testing this page number 1');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
+/* eslint-disable import/prefer-default-export */
 const gameId = 'P2BvfbmisuujDRLJyYxc';
-const name = document.getElementById('username');
-const score = document.getElementById('score');
-const ul = document.getElementById('ul');
+// const name = document.getElementById('username');
+// const score = document.getElementById('score');
+const list = document.getElementById('ul');
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+
+
+
+const getData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const { result } = data;
+  printData(result);
+  return data;
+};
+
+refresh.addEventListener('click', getData);
