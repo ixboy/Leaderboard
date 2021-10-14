ad06bd1b6f2cab5b34465755fdb38290599d4683
ixboy <ismaelixboy@gmail.com>
2021-10-14 08:16:30 +0100

    create main.js file

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const gameId = 'P2BvfbmisuujDRLJyYxc';
+const name = document.getElementById('username');
+const score = document.getElementById('score');
+const ul = document.getElementById('ul');
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
