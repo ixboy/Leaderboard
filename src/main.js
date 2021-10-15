9f43874904b4d989e18032e1bef688d1ade7e40f
ixboy <ismaelixboy@gmail.com>
2021-10-15 04:44:56 +0100

    add functions to get the values and push data to Api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 const gameId = 'P2BvfbmisuujDRLJyYxc';
-// const name = document.getElementById('username');
-// const score = document.getElementById('score');
 const list = document.getElementById('ul');
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
 const submit = document.getElementById('submit');
@@ -9,6 +7,7 @@ const refresh = document.getElementById('refresh');
 
 const printData = (data) => {
   list.innerHTML = ' <li><span>Name</span> <span>score</span></li>';
+
   data.forEach((data) => {
     const listElem = document.createElement('li');
     const spanName = document.createElement('span');
@@ -30,4 +29,23 @@ const getData = async () => {
   return data;
 };
 
+async function pushData(user, score) {
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ user, score }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
+}
+
+function getValues(e) {
+  e.preventDefault();
+  const user = e.target.parentElement.children[1].value;
+  const score = e.target.parentElement.children[2].value;
+  pushData(user, score);
+  e.target.parentElement.children[1].value = '';
+  e.target.parentElement.children[2].value = '';
+}
+
 refresh.addEventListener('click', getData);
+submit.addEventListener('click', getValues);
+window.addEventListener('DOMContentLoaded', getData);
